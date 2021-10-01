597682127e6c014d6a9f92b557c18f90409b4e56
L3af <ohhaon@icloud.com>
2021-10-02 03:12:51 +1300

    Update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
+#!/bin/sh
+
+# Check running as sudo
+if ! [ $(id -u) = 0 ]
+then
+  echo "Please rerun as root"
+  exit
+fi
+
 # Install script
 chmod +x ./src/treeshot
 cp ./src/treeshot /usr/bin/treeshot
 
 # Install man page
-mkdir /usr/local/man/man1
+! [ -d "/usr/local/man/man1" ] && echo "Creating '/usr/local/man/man1'" && mkdir /usr/local/man/man1
+
+echo "Coping and compressing man page"
 cp ./man/treeshot.1 /usr/local/man/man1/treeshot.1
-gzip /usr/local/man/man1/treeshot.1
-mandb
+gzip -f /usr/local/man/man1/treeshot.1 1> /dev/null
+
+echo "Updating man database"
+mandb 1> /dev/null
